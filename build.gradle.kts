846b6eb4f7580acbb8cce5a55b8fa9b060dcb88d
shaul <shaul1991@gmail.com>
2025-03-01 15:28:43 +0900

    add Redis dependencies and configuration to application properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,9 @@ dependencies {
     // Mysql
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("com.mysql:mysql-connector-j")
+
+    // Redis
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
 }
 
 kotlin {
