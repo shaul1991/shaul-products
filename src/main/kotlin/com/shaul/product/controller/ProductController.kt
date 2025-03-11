b7478e4db0fd3a1e0681393ab0f00a10b97b3234
shaul <shaul1991@gmail.com>
2025-03-11 17:40:55 +0900

    update ProductController to use plural endpoint for product API

--- src/main/kotlin/com/shaul/product/controller/ProductController.kt
+++ src/main/kotlin/com/shaul/product/controller/ProductController.kt
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 
 @RestController
-@RequestMapping("/api/v1/product")
+@RequestMapping("/api/v1/products")
 class ProductController(
     private val productService: ProductService,
 ) {
