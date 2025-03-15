c7d18df23f7bc7d6d13ee96eccf9fc7c44e0eee9
shaul <shaul1991@gmail.com>
2025-03-15 23:41:01 +0900

    refactor FileServerClient to use @PostMapping for file upload endpoint

--- src/main/kotlin/com/shaul/product/client/FileServerClient.kt
+++ src/main/kotlin/com/shaul/product/client/FileServerClient.kt
@@ -1,13 +1,12 @@
 package com.shaul.product.client
 
 import org.springframework.cloud.openfeign.FeignClient
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.multipart.MultipartFile
 
 @FeignClient(name = "image-service")
 interface FileServerClient {
-    @RequestMapping(method = [RequestMethod.POST], value = ["/files"], consumes = ["multipart/form-data"])
+    @PostMapping(value = ["/files"], consumes = ["multipart/form-data"])
     fun publicFileUpload(@RequestPart("files") files: List<MultipartFile>): String
 }
